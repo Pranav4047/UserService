8bb2f21c57b7ced90d6a073801540ae0e1f8d501
pranav <pranav4047@gmail.com>
2024-11-20 13:30:25 +0530

    First project commit

---
+++ src/main/java/com/pra/book_my_tickets/user_service/UserService/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pra.book_my_tickets.user_service.UserService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pra/book_my_tickets/user_service/UserService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pra.book_my_tickets.user_service.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
